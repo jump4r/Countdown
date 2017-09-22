fdf1925b57d05437b3c84aa0d37b7d0cb956d28c
jump4r <jump3r@dggaming.net>
2017-09-22 01:13:44 -0500

    Code cleanup.

--- countdown.js
+++ countdown.js
@@ -9,37 +9,16 @@ document.addEventListener('DOMContentLoaded', _ => {
     document.querySelector('#modal-open').addEventListener('click', OpenCreateCountdown);
     document.querySelector('#modal-close').addEventListener('click', CloseCreateCountdown);
     document.querySelector('#modal-submit-button').addEventListener('click', CreateNewCountdown);
-    HTMLCountdownRows = document.querySelectorAll('tr');
-
+    
     // Load the Save Data on load
     LoadData(saveData);
+
+    // Need to call this after we load the data.
+    HTMLCountdownRows = GetCountdownRows();
 });
 
-// Load Data
-// Data is saved as follows: Label, Time When Finished (Date), Time Remaining (int).
-function LoadData(savedData) {
-    let savedDataArray = savedData.split('|');
-    for (let i = 0; i < savedDataArray.length - 1; i++) {
-        // Saved data is in two parts, add to the list
-        if (savedDataArray[i] === "") { 
-            continue; 
-        }
-
-        // Get the current time
-        let savedCountdown = savedDataArray[i].split(',');
-        let difference = GetTimeDifference(savedCountdown[1]);
-        AddCountdown(savedCountdown[0], savedCountdown[1], difference, false);
-    }
-}
-
-// Gets the difference between the end date and the current time.
-function GetTimeDifference(endDate) {
-    // Calculate the end Time and Compile as string
-    let endTimeAsInt = Date.parse(endDate.toString()) / 1000;
-    let currentTime = (new Date()).getTime() / 1000 | 0;
-    console.log(endTimeAsInt, currentTime);
-    let difference = endTimeAsInt - currentTime;
-    return difference;
+function GetCountdownRows() {
+    return document.querySelectorAll('.countdown-row');
 }
 
 // Update
@@ -47,7 +26,7 @@ function UpdateCountdowns() {
     for (let i = 0; i < countdowns.length; i++) {
         countdowns[i].timeRemainingInt--;
         let updateTimeRemainingString = CompileCountdownTime(countdowns[i].timeRemainingInt);
-        HTMLCountdownRows[i+2].querySelectorAll('td')[1].textContent = updateTimeRemainingString; 
+        HTMLCountdownRows[i].querySelector('.countdown-time-remaining').textContent = updateTimeRemainingString; 
         countdowns[i].timeRemainingString = updateTimeRemainingString;
     }
 }
@@ -62,7 +41,12 @@ function CreateNewCountdown() {
 
     AddCountdown(label, endDate, difference, true)
 }
-// Countdown
+
+// Add a countdown to the list
+// label - The Countdown label
+// endDate - When the countdown ends
+// (int) timeRemainingInt - The left remaining in the countdown
+// (bool) isNewCountdown - are we loading the data from a previous session or not?
 function AddCountdown(label, endDate, timeRemainingInt, isNewCountdown) {
     
     let newCountdown = {
@@ -75,8 +59,7 @@ function AddCountdown(label, endDate, timeRemainingInt, isNewCountdown) {
     let timeRemainingString = CompileCountdownTime(timeRemainingInt);
     newCountdown.timeRemainingString = timeRemainingString;
 
-    let t = document.querySelector('#countdown-row');
-
+    let t = document.querySelector('#countdown-template');
     let clone = document.importNode(t.content, true);   
 
     // Add values to HTML
@@ -89,21 +72,19 @@ function AddCountdown(label, endDate, timeRemainingInt, isNewCountdown) {
 
     // Insert the row before the last row
     tr = tb.querySelectorAll("tr");
-
     tb.insertBefore(clone, tr[tr.length-1]);
 
     // Add new countdown to countdowns list
     countdowns.push(newCountdown);
     
-    // If we are creating a new countdown, we need to update save sile
+    // If we are creating a new countdown, we need to update save data and save file
     if (isNewCountdown) {
-        console.log("Save File Updated");
         saveData += UpdateSaveString(newCountdown);
         UpdateSaveData(saveData);
     }
 
     // Update the Countdown Rows
-    HTMLCountdownRows = document.querySelectorAll('tr');
+    HTMLCountdownRows = GetCountdownRows();
 
     // If we weren't updating the countdowns already, do that
     if (boolUpdateCountdowns == false) {
@@ -114,6 +95,7 @@ function AddCountdown(label, endDate, timeRemainingInt, isNewCountdown) {
     CloseCreateCountdown();
 }
 
+// Compile the (int) time (in seconds) to a String 
 function CompileCountdownTime(time) {
     let rtn = "";
     rtn += Math.floor( time / (60 * 60 * 24)).toString() + " Days, ";
@@ -126,22 +108,51 @@ function CompileCountdownTime(time) {
     return rtn;
 }
 
+// Gets the difference between the end date and the current time.
+function GetTimeDifference(endDate) {
+    // Calculate the end Time and Compile as string
+    let endTimeAsInt = Date.parse(endDate.toString()) / 1000;
+    let currentTime = (new Date()).getTime() / 1000 | 0;
+    return (endTimeAsInt - currentTime);
+}
+
 // Open Modal to create a new countdown
 function OpenCreateCountdown() {
     modal.style.display = 'flex';
 }
 
+// Close Modal
 function CloseCreateCountdown() {
     modal.style.display = 'none';
 }
 
+// Update the save string to be stored in localStorage
 function UpdateSaveString(newCountdown) {
     return newCountdown.name + ',' + newCountdown.finishDate + ',' + newCountdown.timeRemainingInt + '|'; 
 }
 
+// Save Data
 function UpdateSaveData(newCountdownSaveString) {
     window.localStorage.setItem("SaveData", saveData);
 }
+
+// Load Data
+// Data is saved as follows: Label, Time When Finished (Date), Time Remaining (int).
+function LoadData(savedData) {
+    let savedDataArray = savedData.split('|');
+    for (let i = 0; i < savedDataArray.length - 1; i++) {
+        // Saved data is in two parts, add to the list
+        if (savedDataArray[i] === "") { 
+            continue; 
+        }
+
+        // Get the current time　difference
+        let savedCountdown = savedDataArray[i].split(',');
+        let difference = GetTimeDifference(savedCountdown[1]);
+        AddCountdown(savedCountdown[0], savedCountdown[1], difference, false);
+    }
+}
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
